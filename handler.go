6c7c0dbc5b1ee35a2160566ddf313e16476b7829
Derek Dowling <ddowling@justin.tv>
2015-12-02 10:46:26 -0800

    Adding link to jsc

--- handler.go
+++ handler.go
@@ -1,3 +1,6 @@
-// Package jsh (JSON API Specification Handler) makes it easy to parse requests
-// and send responses that match the JSON API Specification: http://jsonapi.org/
+// Package jsh (JSON API Specification Handler) makes it easy to parse JSON API
+// requests and send responses that match the JSON API Specification: http://jsonapi.org/
+// from your server.
+//
+// For a request client, see: jsc: https://godoc.org/github.com/derekdowling/go-json-spec-handler/client
 package jsh
